884a725a78624aac3d89b864fb98a02d993d1c56
Petra Kelković <petra.kelkovic@fer.hr>
2023-11-05 00:54:19 +0100

    stranica za registraciju i izmjene kod sendData

--- React/byte-pit/src/pages/Registration.js
+++ React/byte-pit/src/pages/Registration.js
@@ -1,7 +1,45 @@
+import {useState} from 'react';
+import sendData from "../sendData";
+
+//obrazac za registraciju koji šalje podatke klikom na gumb u nasu imaginarnu json bazu
 
 const Registration = () => {
+    const [username, setUsername] = useState('');
+    const [name, setName] = useState('');
+    const [surname, setSurname] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const submitFja = (e) => {
+        e.preventDefault();
+        const user = {name: name, surname: surname, username: username, email: email, password:password};
+
+        sendData('http://localhost:8080/users', user);
+    }
+    
     return (
-        <h1>Registration page</h1>
+        <div className="wrapper">
+            <h1>Registracija:</h1>
+            <form onSubmit = {submitFja}>
+                <label>Ime:
+                    <input type = "text" value = {name} required onChange={(e) => setName(e.target.value)}></input>
+                </label>
+                <label>Prezime:
+                    <input type = "text" value = {surname} required onChange={(e) => setSurname(e.target.value)}></input>
+                </label>
+                <label>Email:
+                    <input type = "text" value = {email} required onChange={(e) => setEmail(e.target.value)}></input>
+                </label>
+                <label>Korisničko ime:
+                    <input type = "text" value = {username} required onChange={(e) => setUsername(e.target.value)}></input>
+                </label>
+                <label>Lozinka:
+                    <input type = "password" value = {password} required onChange={(e) => setPassword(e.target.value)}></input>
+                </label>
+                <button className='submitGumb'>Registriraj me!</button>
+
+            </form>
+        </div>
     );
 }
 
