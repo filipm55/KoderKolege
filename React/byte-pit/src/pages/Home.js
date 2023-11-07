4e854957b4030daaa4ab7406ace7f337257cbf42
filipm55 <filipmohler10@gmail.com>
2023-11-07 17:50:57 +0100

    Dodan kalendar

--- React/byte-pit/src/pages/Home.js
+++ React/byte-pit/src/pages/Home.js
@@ -1,12 +1,15 @@
-import dayjs from "dayjs";
+import React, { useState } from "react";
+import './Home.css';
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
+
 
 const Home = () => {
-    return (
-        <div>
-          <h1>Home page</h1>
-      </div>
-    );
+  return  (
+    <div>
+      <h1>Home Page</h1>
+      <Calendar></Calendar>
+    </div>
+	);
 }
-
 export default Home;
-
