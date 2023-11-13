9d0aa43ad509aa2c661abd28b606138323f4f12b
bpetra1 <bursic.petra1@gmail.com>
2023-11-13 16:51:41 +0100

    Dodani funkcionalni login i logout (Sessions)

--- React/byte-pit/src/components/Navbar.js
+++ React/byte-pit/src/components/Navbar.js
@@ -1,8 +1,29 @@
+import { useState, useEffect } from 'react';
 import "./NavbarStyle.css"
 import logo from "./logo.svg"
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import Cookies from "universal-cookie";
 
 const Navbar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const cookies = new Cookies();
+    const jwtToken = cookies.get('jwt_authorization');
+
+    if (jwtToken) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
+
+  const logout = () => {
+    const cookies = new Cookies();
+    cookies.remove('jwt_authorization');
+
+    window.location.href = '/login'; // Redirect to the login page
+};
 
   return (
     <div className="navbar">
@@ -10,33 +31,41 @@ const Navbar = () => {
         <i className="menu-icon">
           &#9776;
         </i>
-       <Link to='/'> <img src={logo} alt="Logo" className="logo" /></Link>
-        </div>
+        <Link to='/'> <img src={logo} alt="Logo" className="logo" /></Link>
+      </div>
       <div className="omotac">
-      <div className="middle">
-        <p>|</p>
-        <Link to='/calendar' className="nav-link middle-link">
-          KALENDAR NATJECANJA
-        </Link>
-        <p>|</p>
-        <Link to='/users' className="nav-link middle-link">
-          KORISNICI
-        </Link>
-        <p>|</p>
-        <Link to='/tasks' className="nav-link middle-link">
-          ZADACI ZA VJEŽBU
-        </Link>
-        <p>|</p>
-      </div>
+        <div className="middle">
+          <p>|</p>
+          <Link to='/calendar' className="nav-link middle-link">
+            KALENDAR NATJECANJA
+          </Link>
+          <p>|</p>
+          <Link to='/users' className="nav-link middle-link">
+            KORISNICI
+          </Link>
+          <p>|</p>
+          <Link to='/tasks' className="nav-link middle-link">
+            ZADACI ZA VJEŽBU
+          </Link>
+          <p>|</p>
+        </div>
 
-      <div className="right">
-        <Link to='/login' className="nav-link middle-link">
-          PRIJAVA
-        </Link>
-        <Link to='/registration' className="nav-link">
-          REGISTRACIJA
-        </Link>
-      </div>
+        <div className="right">
+          {isLoggedIn ? (
+            <Link to='/login' onClick={logout} className="nav-link middle-link">
+             ODJAVA
+            </Link>
+          ) : (
+            <Link to='/login' className="nav-link middle-link">
+              PRIJAVA
+            </Link>
+          )}
+          {!isLoggedIn && (
+            <Link to='/registration' className="nav-link">
+              REGISTRACIJA
+            </Link>
+          )}
+        </div>
       </div>
     </div>
   );
--- React/byte-pit/src/pages/Login.js
+++ React/byte-pit/src/pages/Login.js
@@ -1,13 +1,24 @@
 import { useState } from 'react';
 import sendData from "../sendData";
 import './Registration.css';
+import Cookies from "universal-cookie"
+//import { useHistory } from 'react-router-dom'; // Import useHistory
 
 const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [message, setMessage] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
+    const [user, setUser] = useState('')
 
+    const cookies = new Cookies();
+
+    /*const logout = () => {
+        setUser('')
+        cookies.remove("jwt_authorization")
+    }*/
+
+    //login
     const submitFja = async (e) => {
         setErrorMessage('')
         setMessage('')
@@ -18,8 +29,17 @@ const Login = () => {
             const response = await sendData('http://localhost:8080/login', loginDTO);
 
             if (typeof response === 'string') {
-                setMessage(response);   //ovdjde je token...treba ga spremiti u localstorage i preko toga radidi sessione
+                //setMessage(response);  
                 setErrorMessage('');
+
+                let token = response;
+                const expirationDate = new Date();
+                expirationDate.setTime(expirationDate.getTime() + (3 * 60 * 60 * 1000)); // stavila sam da traje 3 sata prijava..
+                cookies.set("jwt_authorization", token, {
+                    expires: expirationDate
+                });
+                window.location.href = '/'; // Redirect to the login page
+
             } else {
                 setMessage(response.message);
                 setErrorMessage('');
