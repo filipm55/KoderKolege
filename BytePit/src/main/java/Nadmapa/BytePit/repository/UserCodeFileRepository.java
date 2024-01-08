eb4e67ee201e4a3faf0b2f245fd1f179f58be36e
nivostojic <93402926+nivostojic@users.noreply.github.com>
2024-01-08 19:41:33 +0100

    fajlovi setup

---
+++ BytePit/src/main/java/Nadmapa/BytePit/repository/UserCodeFileRepository.java
@@ -0,0 +1,7 @@
+package Nadmapa.BytePit.repository;
+
+import Nadmapa.BytePit.domain.UserCodeFile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserCodeFileRepository extends JpaRepository<UserCodeFile, Long> {
+}
