277b750c66e821e70d2dd11b60d05386d2459533
filipm55 <filipmohler10@gmail.com>
2024-01-18 22:29:24 +0100

    proba

--- React/byte-pit/src/setupProxy.js
+++ React/byte-pit/src/setupProxy.js
@@ -6,6 +6,11 @@ module.exports = function (app) {
     createProxyMiddleware({
       target: "https://bytepitb.onrender.com/",
       changeOrigin: true,
+      onProxyRes: function (proxyRes, req, res) {
+        res.setHeader('Access-Control-Allow-Origin', 'https://bytepitf.onrender.com');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+      }
     })
   );
 };
