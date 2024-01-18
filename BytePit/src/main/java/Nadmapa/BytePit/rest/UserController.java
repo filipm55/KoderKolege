ef025177f89ced68f3909dc8b763ee02b78ed3f2
Mislav <mk54006@fer.hr>
2024-01-18 23:19:24 +0100

    valjda fix za email

--- BytePit/src/main/java/Nadmapa/BytePit/rest/UserController.java
+++ BytePit/src/main/java/Nadmapa/BytePit/rest/UserController.java
@@ -135,7 +135,7 @@ public class UserController {
             if (user.getUserType() == UserType.COMPETITOR) {
                 message += "\n" +
                         "Hvala vam što ste se registrirali. Vaš račun je još samo potrebno aktivirati preko priloženog linka i onda ste spremni za izazove natjecateljskog programiranja!\n" +
-                        "http://localhost:8080/confirm-registration?hash=" + user.getConfirmationHash() + "&email=" + user.getEmail() + "\n" +
+                        "https://bytepitb.onrender.com/confirm-registration?hash=" + user.getConfirmationHash() + "&email=" + user.getEmail() + "\n" +
                         "Sretno kod rješavanja zadataka i neka kodovi budu u vašu korist!\n" +
                         "\n" +
                         "Tim BytePit" +
@@ -144,7 +144,7 @@ public class UserController {
 
             } else message += "\n" +
                     "Molimo Vas da potvrdite račun preko ovog linka \n" +
-                    "http://localhost:8080/confirm-registration?hash=" + user.getConfirmationHash() + "&email=" + user.getEmail() + "\n\n" +
+                    "https://bytepitb.onrender.com/confirm-registration?hash=" + user.getConfirmationHash() + "&email=" + user.getEmail() + "\n\n" +
                     "Međutim fali nam još samo jedan korak do cilja.Molimo pričekajte da Vas administrator potvrdi kao voditelja.\n" +
                     "Radujemo se našoj suradnji\n" +
                     "Tim BytePit";
