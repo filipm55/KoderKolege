dde6d67288d4470d2b4e07d7639d1c99744ccb55
filipm55 <filipmohler10@gmail.com>
2024-01-19 00:56:39 +0100

    opet proxy

--- React/byte-pit/src/setupProxy.js
+++ React/byte-pit/src/setupProxy.js
@@ -7,7 +7,7 @@ module.exports = function (app) {
       target: "https://bytepitb-myjy.onrender.com//",
       changeOrigin: true,
       onProxyRes: function (proxyRes, req, res) {
-        res.setHeader('Access-Control-Allow-Origin', 'https://bytepitf.onrender.com');
+        res.setHeader('Access-Control-Allow-Origin', 'https://bytepit-gxla.onrender.com/');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
       }
