50569d2703f5ec5d76e31c6e1fe43a60a43f6410
Mislav <mk54006@fer.hr>
2024-01-19 15:54:02 +0100

    dioooo testova (jos bi htio dodat 1, i fixat 7.) ako stignem

--- BytePit/src/test/java/Nadmapa/BytePit/BytePitApplicationTests.java
+++ BytePit/src/test/java/Nadmapa/BytePit/BytePitApplicationTests.java
@@ -1,32 +1,25 @@
 package Nadmapa.BytePit;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
 import Nadmapa.BytePit.domain.*;
 import Nadmapa.BytePit.repository.UserRepository;
 import Nadmapa.BytePit.rest.UserController;
-import Nadmapa.BytePit.service.UserService;
-import Nadmapa.BytePit.service.impl.UserRegistrationService;
-import Nadmapa.BytePit.service.impl.UserServiceJpa;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-
 import org.junit.jupiter.api.Test;
+
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -34,23 +27,29 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-@SpringBootTest @ExtendWith(MockitoExtension.class) @AutoConfigureMockMvc
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class BytePitApplicationTests {
-	@Mock
+	@Autowired
 	private UserRepository userRepository;
 
-	@InjectMocks
+	@Autowired
+	private MockMvc mockMvc;
+
+
+
 	private UserController userController;
 
-	private MockMvc mockMvc;
-	 /* @BeforeEach
-	void setup(){
-		userRepository= new
-		user = new User("korisnik1", "ime", "prezime", "lozinka", "ime.prezime@gmail.com", null, "COMPETITOR");
-		userService = new UserServiceJpa();
-		userService.saveUser(user);
-	}*/
 
+	//Provjeravamo rade li osnovne funkcije razreda User (njih sve ostale funkcije koriste)
+	//Konkretno u ovom testu provjeravamo hoće li naš razred u konstruktoru dobro "zapamtiti" vrijednosti parametara, koje kasnije dohvaćamo get metodama, to se npr koristi kada se vrši registracija
 	@Test
 	void getterTest(){
 		User user = new User("korisnik1", "ime", "prezime", "lozinka", "ime.prezime@gmail.com", null, "COMPETITOR");
@@ -63,7 +62,8 @@ class BytePitApplicationTests {
 		assertEquals(UserType.COMPETITOR, user.getUserType());
 
 	}
-
+	//Provjeravamo rade li osnovne funkcije razreda User (njih sve ostale funkcije koriste)
+	//Konrektno u ovom primjeru provjeravamo možemo li set metodama razreda User mijenjati njegove atribute (to se npr koristi kada se mijenjaju podaci od korisnika na natjecanju)
 	@Test
 	void setterTest(){
 		User user = new User("korisnik1", "ime", "prezime", "lozinka", "ime.prezime@gmail.com", null, "COMPETITOR");
@@ -79,48 +79,138 @@ class BytePitApplicationTests {
 		assertEquals(UserType.COMPETITOR, user.getUserType());
 	}
 
+	//Provjeravamo rade li osnove funkcije razreda Problem i Competition (njih sve ostale funkcije koriste)
+	//Konkretno u ovom primjeru provjeravamo možemo li stvoriti novi problem(tj zadatak) i novi Competition i dodati problem u competition (to se koristi kada se stvaraju natjecanja i dodaju se koji će zadaci biti dio tog natjecanja)
+
 	@Test
-	void competitionProblems(){
-		//String problemMakerId, String title, int points, String duration, String text,Map<String, String> inputOutputExamples, Boolean isPrivate, ProblemType problemType
+	void competitionGetProblemsTest(){
 		Map<String,String> mapa = new HashMap<>();
 		mapa.put("ulaz", "izlaz");
 
 		Problem problem = new Problem("1", "naslov", 5, "10:00", "text" , mapa, false, ProblemType.HARD);
-		//(User competitionMaker, LocalDateTime dateTimeOfBeginning, LocalDateTime dateTimeOfEnding, Set<Problem> problems, Image trophyPicture, boolean slicica_pehara, Boolean isvirtual
 		User user = new User("korisnik1", "ime", "prezime", "lozinka", "ime.prezime@gmail.com", null, "COMPETITION_LEADER");
 		Set<Problem> set = new HashSet<>();
 		set.add(problem);
 		Competition competition = new Competition(user, LocalDateTime.now(), LocalDateTime.now(), set, null, false, false);
 		assertEquals(set, competition.getProblems());
 	}
-
-	/*
+	//Provjeravamo slučaj kada neko natjecanje nema sličicu trofeja
+	//To se koristi kada prikazujemo statistiku korisnika (ako nema sličice natjecanja, onda se prikazuje neka "default" ikona pehara
 	@Test
-	void testGetUserById() throws Exception {
-		// Id koji ćete koristiti u testu
-		Long userId = 1L;
+	void competitionNoTrophyPictureThrowsTest(){
+		Map<String,String> mapa = new HashMap<>();
+		mapa.put("ulaz", "izlaz");
 
-		// Mock User objekt koji će se koristiti kao rezultat
-		User mockUser = new User();
-		mockUser.setId(userId);
-		mockUser.setUsername("testUser");
-
-		// Postavljanje ponašanja mock repositoryja
-		when(userRepository.findById(userId)).thenReturn(java.util.Optional.ofNullable(mockUser));
-
-		// Testiranje HTTP GET zahtjeva
-		mockMvc.perform(get("/api/users/{id}", userId)
-						.contentType(MediaType.APPLICATION_JSON))
+		Problem problem = new Problem("1", "naslov", 5, "10:00", "text" , mapa, false, ProblemType.HARD);
+		User user = new User("korisnik1", "ime", "prezime", "lozinka", "ime.prezime@gmail.com", null, "COMPETITION_LEADER");
+		Set<Problem> set = new HashSet<>();
+		set.add(problem);
+		Competition competition = new Competition(user, LocalDateTime.now(), LocalDateTime.now(), set, null, false, false);
+		assertThrows(NullPointerException.class, () ->  competition.getTrophyPicture().getData());
+	}
+	//Provjeravamo kompleksnije funkcije sustava
+	//Konkretno provjeravamo možemo li iz baze podataka izvući nekog usera
+	//Provjeru radimo uspoređivanjem savedUser.id (on je u bazi) i jsonNode.get("id") <- jsonNode je objekt koji iz predstavlja odgovor na naš get zahtjev na server (metoda bi trebala iz baze povući usera sa nekim id-em)
+	//Dakle gledamo hoće li get zahtjev vratiti točan podatak iz baze
+	@Test
+	@DirtiesContext
+	void testGetUserById() throws Exception{
+		User user = new User("korisnik1", "ime", "prezime", "lozinka", "ime.prezime@gmail.com", null, "COMPETITION_LEADER");
+		userRepository.delete(user);
+		User savedUser = userRepository.save(user);
+		MvcResult result = mockMvc.perform(get("/users/byId/{id}", savedUser.getId()))
 				.andExpect(status().isOk())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("$.id").value(userId.intValue()))
-				.andExpect(jsonPath("$.username").value("testUser"))
-				.andExpect(result -> {
-					User returnedUser = new ObjectMapper().readValue(result.getResponse().getContentAsString(), User.class);
-					assertNotNull(returnedUser);
-					assertEquals(userId, returnedUser.getId());
-					assertEquals("testUser", returnedUser.getUsername());
-				});
-	}*/
+				.andReturn();
+		userRepository.deleteById(savedUser.getId());
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		JsonNode jsonNode = objectMapper.readTree(result.getResponse().getContentAsString());
+
+		assertEquals(savedUser.getId(), jsonNode.get("id").asLong());
+	}
+	//Provjeravamo radi li put metoda za stvaranje korisnika (koristi se prilikom registracije)
+	//Slično kao i u prošlom primjeru provjeravamo hoće li nam get metoda vratiti dobrog korisnika, kojeg ovog puta ne upisujemo direktno u bazu, već put metodom
+	@Test
+	@DirtiesContext
+	void createUserTest() throws Exception{
+		MockMultipartFile file = new MockMultipartFile(
+				"image",           // Parametar "image" iz metode
+				"filename.jpg",    // Naziv datoteke
+				MediaType.IMAGE_JPEG_VALUE, // Tip datoteke
+				"file content".getBytes()   // Sadržaj datoteke
+		);
+
+		MvcResult resultFromCreateUser = mockMvc.perform(MockMvcRequestBuilders.multipart("/users")
+						.file(file)
+						.param("name", "ime2")
+						.param("lastname", "prezime2")
+						.param("username", "korisnik2")
+						.param("email", "ime.prezime2@gmail.com")
+						.param("password", "lozinka2")
+						.param("userType", "COMPETITION_LEADER"))
+
+				.andExpect(status().isOk())
+				.andReturn();
+
+		User userFromDatabase = userRepository.findByUsername("korisnik2");
+
+		MvcResult resultFromGetUser = mockMvc.perform(get("/users/byId/{id}", userFromDatabase.getId()))
+				.andExpect(status().isOk())
+				.andReturn();
+
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		JsonNode jsonNode = objectMapper.readTree(resultFromGetUser.getResponse().getContentAsString());
+		userRepository.delete(userFromDatabase);
+		assertEquals(userFromDatabase.getId(), jsonNode.get("id").asLong());
+	}
+	//Provjeravamo uvjete za stvaranje korisnika
+	//Konkretno ovdje pokušavamo stvoriti 2 korisnika sa istim usernameom
+	//Provjeravamo hoće li nam server vratiti neki error
+	@Test
+	void uniqueUsernameTest() throws  Exception{
+		MockMultipartFile file = new MockMultipartFile(
+				"image",           // Parametar "image" iz metode
+				"filename.jpg",    // Naziv datoteke
+				MediaType.IMAGE_JPEG_VALUE, // Tip datoteke
+				"file content".getBytes()   // Sadržaj datoteke
+		);
+
+		MvcResult resultFromCreateUser = mockMvc.perform(MockMvcRequestBuilders.multipart("/users")
+						.file(file)
+						.param("name", "ime3")
+						.param("lastname", "prezime3")
+						.param("username", "korisnik3")
+						.param("email", "ime.prezime3@gmail.com")
+						.param("password", "lozinka3")
+						.param("userType", "COMPETITION_LEADER"))
+
+				.andExpect(status().isOk())
+				.andReturn();
+		MvcResult resultFromCreateUser2 = mockMvc.perform(MockMvcRequestBuilders.multipart("/users")
+						.file(file)
+						.param("name", "ime3")
+						.param("lastname", "prezime3")
+						.param("username", "korisnik3")
+						.param("email", "ime.prezime4@gmail.com")
+						.param("password", "lozinka3")
+						.param("userType", "COMPETITION_LEADER"))
+				.andReturn();
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		//JsonNode jsonNode = objectMapper.readTree(resultFromCreateUser.getResponse().getContentAsString());
+		//userRepository.deleteById(jsonNode.get("id").asLong());
+
+		assertTrue(resultFromCreateUser2.getResponse().getStatus() == 400);
+
+	}
+
+
+
+
+
+
+
+
 
 }
